ca02a4edbd7212318eba940cfa723c026ecf3fa4
Parsa P <Parsapoosti@gmail.com>
2026-09-14 14:22:58 -0700

    Prioritize Pistachio wallet in AppKit

---
+++ src/web3/appKitOptions.js
@@ -0,0 +1,9 @@
+export const APPKIT_CONNECTOR_TYPE_ORDER = Object.freeze([
+    'external',
+    'walletConnect',
+    'recent',
+    'injected',
+    'featured',
+    'custom',
+    'recommended',
+])
