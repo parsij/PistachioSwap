38aa47043af70e95d568569858e84e8c7823c02f
Parsa P <Parsapoosti@gmail.com>
2026-09-14 20:48:04 -0700

    Load wallet Ubuntu typography

--- src/features/wallet/components/WalletConnectionButton.jsx
+++ src/features/wallet/components/WalletConnectionButton.jsx
@@ -5,6 +5,7 @@ import WalletAccountDialog from './wallet/WalletAccountDialog.jsx'
 import './wallet/wallet.css'
 import './wallet/sendAssetDialog.css'
 import './wallet/walletAccount.mobile.css'
+import './wallet/walletTypography.css'
 import './wallet/walletSheetDrag.js'
 
 /**
