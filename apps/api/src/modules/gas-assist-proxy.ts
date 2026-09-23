30035650fe7333f5d7c60915c6711097a06fd39e
Parsa P <Parsapoosti@gmail.com>
2026-09-23 14:46:46 -0700

    Allow exact authenticated public-hash Paymaster reports through Gas-Assist proxy

--- apps/api/src/modules/gas-assist-proxy.ts
+++ apps/api/src/modules/gas-assist-proxy.ts
@@ -28,6 +28,8 @@ const PUBLIC_PROXY_ROUTES = Object.freeze([
     ['POST', new RegExp(`^/v1/sponsorship/orders/${SAFE_PATH_SEGMENT}/atomic/prepare$`, 'u')],
     ['POST', new RegExp(`^/v1/sponsorship/orders/${SAFE_PATH_SEGMENT}/paymaster/stub$`, 'u')],
     ['POST', new RegExp(`^/v1/sponsorship/orders/${SAFE_PATH_SEGMENT}/paymaster/sponsor$`, 'u')],
+    ['POST', new RegExp(`^/v1/sponsorship/orders/${SAFE_PATH_SEGMENT}/paymaster/submitted$`, 'u')],
+    ['POST', new RegExp(`^/v1/sponsorship/orders/${SAFE_PATH_SEGMENT}/paymaster/receipt$`, 'u')],
     ['POST', /^\/v1\/sponsorship\/particle\/before-paymaster-sign$/u],
 ] as const)
 
